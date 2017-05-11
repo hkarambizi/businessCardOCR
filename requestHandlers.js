e2dcf80b1eb6f51480c3f8485e01adb4ed8fff9d
lmegviar <lmegviar@gmail.com>
2017-05-11 11:56:47 -0400

    Add first draft of request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,58 @@
+var getContactInfo = (req, res) => {
+  var document = req.query.doc;
+  console.log('doc: ', document);
+  var splitDoc = document.toLowerCase().split(/\r?\n/);
+  var phone = undefined;
+  var name = undefined;
+  var nameChars = '';
+  var nameReg = ''
+  var phoneReg = /(\d*)(.*\d+)(.*\d+){3}(.*\d+){4}/;
+  var emailReg = /(\w+)@(\w+).(\w+)/g
+  var email = document.match(emailReg)[0];
+
+  if (email) {
+    nameChars = email.toLowerCase().split('@')[0].replace(/\W/g, '');
+    nameChars.split('').forEach((char, i) => {
+      nameReg += '(' + char + ').*';
+    });
+    nameReg = new RegExp(nameReg);
+  }
+
+  splitDoc.forEach((line, i) => {
+    if (line.match(phoneReg)!== null && !line.includes('f')){
+      phone = line.match(phoneReg)[0].replace(/\D/g,'');
+    }
+    if (line.match(nameReg) !== null && !line.includes('@')) {
+      name = line.match(nameReg)[0];
+      name = name.replace(/\b\w/g,(char) => {return char.toUpperCase();}).trim();
+    }
+  });
+
+  res.send({Name: name, Phone: phone, Email: email});
+};
+
+var getName = (req, res) => {
+  var document = req.query.doc;
+  var contactInfo = getContactInfo(document);
+  res.send('Name: ' + contactInfo.Name);
+};
+
+var getPhoneNumber = (req, res) => {
+  var document = req.query.doc;
+  var contactInfo = getContactInfo(document);
+  res.send('Phone: ' + contactInfo.Phone);
+};
+
+var getEmailAddress = (req, res) => {
+  var document = req.query.doc;
+  var contactInfo = getContactInfo(document);
+  res.send('Email: ' + contactInfo.Email);
+};
+
+module.exports = {
+  getContactInfo: getContactInfo,
+  getName: getName,
+  getPhoneNumber: getPhoneNumber,
+  getEmailAddress: getEmailAddress
+}
+
